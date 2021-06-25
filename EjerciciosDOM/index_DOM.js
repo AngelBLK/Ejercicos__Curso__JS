9c934ef729dd7a4003ca10776e8de2e239d48b91
AngelBLK <jaidenze@gmail.com>
2021-06-24 22:16:18 -0500

    Test Responsivo v1.0

--- EjerciciosDOM/index_DOM.js
+++ EjerciciosDOM/index_DOM.js
@@ -3,6 +3,7 @@ import hamburgerMenu from "./burger.js";
 import countdouwn from "./cuenta_regresiva.js";
 import darkTheme from "./dark-theme.js";
 import responsiveMedia from "./objeto_responsive.js";
+import responsiveTester from "./prueba_responsive.js";
 import { digitalClock, alarm } from "./reloj-audio.js";
 import {moveBall} from "./teclado.js";
 
@@ -45,6 +46,8 @@ d.addEventListener("DOMContentLoaded", (e) =>{
         `<a href="https://goo.gl/maps/faVoLWP6qGg1Bz3b7" target="_blank" rel="noopener">Ver Mapa</a>`, 
         `<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7525.32326366452!2d-99.17196599496774!3d19.42702054780647!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85d1ff35f5bd1563%3A0x6c366f0e2de02ff7!2sEl%20%C3%81ngel%20de%20la%20Independencia!5e0!3m2!1ses-419!2smx!4v1624410919761!5m2!1ses-419!2smx" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>`
     );
+
+    responsiveTester("responsve-tester");
     
 });
 /** keydown: en cuento se presiona la tecla.
---
+++ EjerciciosDOM/prueba_responsive.js
@@ -0,0 +1,21 @@
+const d = document;
+
+export default function responsiveTester(form){
+    const $form = d.getElementById(form);
+
+    let tester;
+    
+    d.addEventListener("submit", (e )=> {
+        if(e.target === $form){
+            e.preventDefault();
+            tester = window.open(
+                $form.direccion.value, 
+                "Tester",`innerWidth=${$form.ancho.value}, innerHeigth=${$form.alto.value}`);
+        }
+    });
+
+    d.addEventListener("click", (e) =>{
+        if(e.target === $form.cerrar) tester.close;
+        
+    })
+}
